8de02387c8074e873230bc6ad1a2177c03401161
0xDani <daniele.fallaci@gmail.com>
2025-04-03 20:17:24 +0200

    Buttons, Door and basi Trap V0 (no graphic)

---
+++ CatsAndRats/Assets/Scripts/Door.cs
@@ -0,0 +1,47 @@
+using UnityEditor.UI;
+using UnityEngine;
+
+public class Door : MonoBehaviour, IAction
+{
+    [SerializeField] private float openAngle = 90f; // Angolo di apertura
+    [SerializeField] private float speed = 2f; // Velocità di apertura
+    private bool isOpen = false;
+    private Quaternion closedRotation;
+    private Quaternion openRotation;
+    public void action()
+    {
+        Debug.Log("porta azionata");
+        if (!isOpen)
+        {
+            StartCoroutine(openDoor());
+        }   
+    }
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+     closedRotation = transform.rotation;
+     openRotation = Quaternion.Euler(0, openAngle, 0) * closedRotation;   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private System.Collections.IEnumerator openDoor()
+    {
+        isOpen = true;
+        float elapsedTime = 0f;
+        float duration = 1f / speed; // Tempo necessario per completare l'animazione
+
+        while (elapsedTime < duration)
+        {
+            transform.rotation = Quaternion.Lerp(closedRotation, openRotation, elapsedTime / duration);
+            elapsedTime += Time.deltaTime;
+            yield return null;
+        }
+
+        transform.rotation = openRotation; // Assicura che la porta finisca esattamente nell'angolo giusto
+    }
+}
