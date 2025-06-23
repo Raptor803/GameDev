df7ca70b690a7dc567f442011b82f8ece8084f2b
Raptor803 <gionatan9703@gmail.com>
2025-06-23 21:27:15 +0200

    first beta

--- CatsAndRats/Assets/Scripts/MyGame/Camera/CameraFollow.cs
+++ CatsAndRats/Assets/Scripts/MyGame/Camera/CameraFollow.cs
@@ -6,10 +6,17 @@ namespace MyGame
     public class CameraFollow : MonoBehaviour
     {
         public Transform target;
+        public bool followX = true;
+        public bool followY = true;
+        public bool followZ = true;
+        public Vector3 offset = new Vector3(0, 0, 0);
 
         void Update()
         {
-            transform.position = new Vector3(target.position.x, transform.position.y, transform.position.z);
+            transform.position = new Vector3(
+                followX ? target.position.x + offset.x : transform.position.x,
+                followY ? target.position.y + offset.y : transform.position.y,
+                followZ ? target.position.z + offset.z : transform.position.z);
         }
     }
 }
